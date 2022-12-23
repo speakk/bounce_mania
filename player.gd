16154cc564b54a70053cf476348254b47d3e140d
speak <speakk@gmail.com>
2022-12-23 12:46:37 +0200

    Bigger world, correct bounce direction

--- player.gd
+++ player.gd
@@ -1,6 +1,4 @@
-extends Node2D
-
-class_name Player
+extends RigidBody2D
 
 @export var speed: int = 1200
 @export var bounce_speed: int = 1000
@@ -27,7 +25,7 @@ func get_input():
 	velocity = velocity.normalized() * speed
 	
 func _ready():
-	$PhysicsBody/Sprite2D.frame = 0
+	$Sprite2D.frame = 0
 	bounce_timer = Timer.new()
 	add_child(bounce_timer)
 
@@ -35,45 +33,46 @@ var max_dist = 2
 
 func direct_eye(eye, offset):
 	var eye_pos = eye.global_position
-	var player_pos = $PhysicsBody.position + offset
+	var player_pos = position + offset
 	var dir = eye_pos.direction_to(get_global_mouse_position())
-	dir = dir.rotated(-$PhysicsBody.rotation)
+	dir = dir.rotated(-rotation)
 	var dist = eye_pos.distance_to(get_local_mouse_position())
 	eye.position = dir * min(dist, max_dist) + offset
 
 func direct_eyes():
 	var y_offset = 5
-	direct_eye($PhysicsBody/LeftEyeSprite, Vector2(-8, y_offset))
-	direct_eye($PhysicsBody/RightEyeSprite, Vector2(7, y_offset))
+	direct_eye($LeftEyeSprite, Vector2(-8, y_offset))
+	direct_eye($RightEyeSprite, Vector2(7, y_offset))
 
 func _process(delta):
 	get_input()
-	$PhysicsBody.apply_force(velocity)
+	apply_force(velocity)
 	direct_eyes()
 
 	if Input.is_action_just_pressed("bounce"):
-		var direction = (get_local_mouse_position() - $PhysicsBody.position).normalized()
+		var direction = (get_global_mouse_position() - position).normalized()
 		var vector = direction * bounce_speed
 		bounce(vector)
 	
 	
 func bounce(direction):
 	if not bouncing:
-		$PhysicsBody.apply_impulse(direction)
-		$PhysicsBody/Sprite2D.frame = 1
+		#linear_velocity = Vector2(0, 0)
+		apply_impulse(direction)
+		$Sprite2D.frame = 1
 		bouncing = true
 		bounce_timer.start(bounce_time)
-		$PhysicsBody/TrailParticles.emitting = true
+		$TrailParticles.emitting = true
 		current_damage = damage * 2
-		$PhysicsBody.set_collision_mask_value(2, false)
+		set_collision_mask_value(2, false)
 		
 		await bounce_timer.timeout
 		
 		bouncing = false
-		$PhysicsBody/Sprite2D.frame = 0
-		$PhysicsBody/TrailParticles.emitting = false
+		$Sprite2D.frame = 0
+		$TrailParticles.emitting = false
 		current_damage = damage
-		$PhysicsBody.set_collision_mask_value(2, true)
+		set_collision_mask_value(2, true)
 		
 		
 
@@ -85,6 +84,7 @@ func bounce(direction):
 #		Events.brick_hit.emit(body, self, self.current_damage)
 
 
+
 func _on_area_2d_body_entered(body):
 	print("Entered?")
 	if body.is_in_group("bricks"):
@@ -92,6 +92,7 @@ func _on_area_2d_body_entered(body):
 		#Events.emit_signal("brick_hit", self, self.current_damage)
 		Events.brick_hit.emit(body, self, self.current_damage)
 	
-	$PhysicsBody/Sprite2D.scale = Vector2(1.4, 1.4)
+	$Sprite2D.scale = Vector2(1.4, 1.4)
+	#$/Sprite2D.material.
 	var tween = get_tree().create_tween()
-	tween.tween_property(get_node("PhysicsBody/Sprite2D"), "scale", Vector2(1, 1), 0.2)
+	tween.tween_property(get_node("Sprite2D"), "scale", Vector2(1, 1), 0.2)
