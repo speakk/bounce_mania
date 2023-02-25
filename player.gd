6bd291e82bd11adb5ef84c908f480f8a732efb0f
speak <speakk@gmail.com>
2023-02-25 23:14:14 +0200

    A bit more trail particles

--- player.gd
+++ player.gd
@@ -66,10 +66,10 @@ func get_input():
 		velocity.x += 1
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
+#	if Input.is_action_pressed("down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("up"):
+#		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 	
 	if Input.is_action_just_pressed("bounce"):
